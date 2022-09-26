ac6eadc09e794d109648ecb61337facfd2a21353
Robert Krantz <robert.krantz@gmail.com>
2022-09-26 11:24:04 +0200

    Added a final meter value to be sent on stop transaction

--- src/main/java/de/rwth/idsg/steve/integration/IntegrationService.java
+++ src/main/java/de/rwth/idsg/steve/integration/IntegrationService.java
@@ -6,4 +6,6 @@ import ocpp.cs._2015._10.MeterValuesRequest;
 public interface IntegrationService {
     void meterValues(String chargeBoxIdentity, MeterValuesRequest request);
     void chargingBoxStatus(String chargeBoxIdentity, int connectorIdentity, ConnectorStatus status);
+
+    void onStopTransaction(String chargeBoxIdentity, int transactionId);
 }
