47eca7a00c96a057d9c05b502f6e4a79e4512d55
Robert Krantz <robert.krantz@gmail.com>
2022-10-05 08:49:42 +0200

    Added new methods for start and stop transaction. (#24)

--- src/main/java/de/rwth/idsg/steve/integration/IntegrationService.java
+++ src/main/java/de/rwth/idsg/steve/integration/IntegrationService.java
@@ -2,10 +2,12 @@ package de.rwth.idsg.steve.integration;
 
 import de.rwth.idsg.steve.integration.dto.ConnectorStatus;
 import ocpp.cs._2015._10.MeterValuesRequest;
+import ocpp.cs._2015._10.StartTransactionRequest;
+import ocpp.cs._2015._10.StopTransactionRequest;
 
 public interface IntegrationService {
     void meterValues(String chargeBoxIdentity, MeterValuesRequest request);
     void chargingBoxStatus(String chargeBoxIdentity, int connectorIdentity, ConnectorStatus status);
-
-    void onStopTransaction(String chargeBoxIdentity, int transactionId);
+    void onStartTransaction(String chargeBoxIdentity, StartTransactionRequest startTransactionRequest);
+    void onStopTransaction(String chargeBoxIdentity, StopTransactionRequest stopTransactionRequest);
 }
